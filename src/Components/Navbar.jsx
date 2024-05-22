cd74402d0b5fe91e7951b66eb2ff94a89addad29
sixeyes198 <yossiguadie@icloud.com>
2024-05-22 18:03:09 +0300

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,8 +2,8 @@ const Navbar = () => {
   return (
     <>
       <header className="bg-gray-800 text-white">
-        <nav className="container mx-auto p-4 flex justify-between items-center">
-          <ul className="flex space-x-4">
+        <nav className="container mx-auto p-4 flex flex-col md:flex-row justify-between items-center">
+          <ul className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
             <li>
               <a href="#" className="hover:text-gray-300">
                 Home
@@ -25,7 +25,7 @@ const Navbar = () => {
               </a>
             </li>
           </ul>
-          <div className="header-buttons">
+          <div className="header-buttons mt-2 md:mt-0">
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
               Email
             </button>
