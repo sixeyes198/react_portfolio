6afd7c7cbecafb394f19d41f9a5562e023d1dc94
sixeyes198 <yossiguadie@icloud.com>
2024-07-07 15:09:01 +0300

    update

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,33 +2,33 @@ import pic from "../assets/pic.jpg";
 const Hero = () => {
   return (
     <>
-      <div class="container mx-auto p-6 flex flex-col md:flex-row items-center space-y-6 md:space-y-0 md:space-x-6">
+      <div className="container mx-auto p-6 flex flex-col md:flex-row items-center space-y-6 md:space-y-0 md:space-x-6">
         {/* <!-- icons container --> */}
-        <ul class="flex space-x-4">
+        <ul className="flex space-x-4">
           <li>
             <a
-              class="text-white hover:text-gray-400"
+              className="text-white hover:text-gray-400"
               href="https://github.com/sixeyes198"
               target="_blank"
             >
-              <i class="bx bxl-github text-2xl"></i>
+              <i className="bx bxl-github text-2xl"></i>
             </a>
           </li>
           <li>
             <a
-              class="text-white hover:text-gray-400"
+              className="text-white hover:text-gray-400"
               href="https://www.linkedin.com/in/yossi-guadie-78b30a273/"
               target="_blank"
             >
-              <i class="bx bxl-linkedin-square text-2xl"></i>
+              <i className="bx bxl-linkedin-square text-2xl"></i>
             </a>
           </li>
         </ul>
 
-        <div class="text-center md:text-left">
-          <h2 class="text-2xl font-bold tracking-wide">Yossi guadie</h2>
-          <p class="tracking-wide">Fullstack-Developer</p>
-          <p class="mt-4">
+        <div className="text-center md:text-left">
+          <h2 className="text-2xl font-bold tracking-wide">Yossi guadie</h2>
+          <p className="tracking-wide">Fullstack-Developer</p>
+          <p className="mt-4">
             As a developer with a passion for creating seamless, user-friendly
             applications, I specialize in both front-end and back-end
             development. My expertise lies in utilizing modern frameworks like
@@ -37,11 +37,11 @@ const Hero = () => {
         </div>
 
         {/* <!-- adding image --> */}
-        <div class="flex-shrink-0">
+        <div className="flex-shrink-0">
           <img
             src={pic}
             alt="my profile image"
-            class="w-64 h-64 rounded-full object-cover"
+            className="w-64 h-64 rounded-full object-cover"
           />
         </div>
       </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -54,54 +54,50 @@ const Navbar = () => {
   };
 
   return (
-    <>
-      <header className="bg-gray-800 text-white">
-        <nav className="container mx-auto p-4">
-          {/* Hamburger menu icon for small screens */}
-          <div className="flex justify-between items-center">
-            <div>
-              <FaBars
-                className="text-white cursor-pointer md:hidden"
-                onClick={toggleMenu}
-              />
-            </div>
-            {/* Menu items */}
-            <ul
-              className={`md:flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 ${
-                isOpen ? "block" : "hidden"
-              }`}
-            >
-              <li>
-                <a href="#" className="hover:text-gray-300">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-gray-300">
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-gray-300">
-                  Projects
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-gray-300">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-          {/* Email button */}
-          <div className="header-buttons mt-2 md:mt-0">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              Email
-            </button>
-          </div>
-        </nav>
-      </header>
-    </>
+    <header className="bg-gray-800 text-white">
+      <nav className="container mx-auto p-4 flex justify-between items-center">
+        {/* Hamburger menu icon for small screens */}
+        <div className="flex items-center">
+          <FaBars
+            className="text-white cursor-pointer md:hidden"
+            onClick={toggleMenu}
+          />
+          {/* Menu items */}
+          <ul
+            className={`absolute md:relative top-12 left-0 md:top-auto md:left-auto md:flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 bg-gray-600 md:bg-transparent w-full md:w-auto ${
+              isOpen ? "block" : "hidden"
+            }`}
+          >
+            <li>
+              <a href="#" className="hover:text-gray-300">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-gray-300">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-gray-300">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-gray-300">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        {/* Email button */}
+        <div className="mt-2 md:mt-0">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Email
+          </button>
+        </div>
+      </nav>
+    </header>
   );
 };
 
