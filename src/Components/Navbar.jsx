1bd93f52e300b744d96ded179fbebdf7903f9ba1
sixeyes198 <yossiguadie@icloud.com>
2024-08-23 14:55:36 +0300

    styyling updates

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,37 +9,49 @@ const Navbar = () => {
   };
 
   return (
-    <header className="bg-cyan-700 text-black py-2">
+    <header className=" py-2">
       <nav className="container mx-auto  flex justify-between items-center">
         {/* Hamburger menu icon for small screens */}
         <div className="flex items-center">
           <FaBars
-            className="text-white cursor-pointer md:hidden"
+            className="text-black cursor-pointer md:hidden"
             onClick={toggleMenu}
           />
           {/* Menu items */}
           <ul
-            className={`absolute md:relative top-10 left-0 md:top-auto md:left-auto md:flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 bg-cyan-500 bg-opacity-80 md:bg-transparent w-1/2 h-4/6 md:w-auto ${
+            className={`absolute md:relative top-10 left-0 md:top-auto md:left-auto md:flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 bg-slate-200 bg-opacity-80 md:bg-transparent w-1/2 h-4/6 md:w-auto ${
               isOpen ? "block" : "hidden"
             }`}
           >
             <li>
-              <a href="#" className="block p-2 hover:text-white">
+              <a
+                href="#"
+                className="block p-2 hover:text-white bg-opacity-50 hover:bg-slate-300 rounded-md"
+              >
                 Home
               </a>
             </li>
             <li>
-              <a href="#" className="block p-2 hover:text-white">
+              <a
+                href="#"
+                className="block p-2 hover:text-white bg-opacity-50 hover:bg-slate-300 rounded-md"
+              >
                 About
               </a>
             </li>
             <li>
-              <a href="#" className="block p-2 hover:text-white">
+              <a
+                href="#"
+                className="block p-2 hover:text-white bg-opacity-50 hover:bg-slate-300 rounded-md"
+              >
                 Projects
               </a>
             </li>
             <li>
-              <a href="#" className="block p-2 hover:text-white">
+              <a
+                href="#"
+                className="block p-2 hover:text-white bg-opacity-50 hover:bg-slate-300 rounded-md"
+              >
                 Contact
               </a>
             </li>
@@ -47,7 +59,7 @@ const Navbar = () => {
         </div>
         {/* Email button */}
         <div className="mt-2 md:mt-0">
-          <button className="rounded-full bg-slate-800 hover:bg-cyan-300 text-gray-50 font-bold py-2 px-4 ">
+          <button className="rounded-full bg-slate-800 hover:bg-slate-200 text-white hover:text-black font-bold py-2 px-4 ">
             Email
           </button>
         </div>
